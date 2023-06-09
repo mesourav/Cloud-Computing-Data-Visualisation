bb11139e9e2b0487d3cd2f3b66683b0f2b874909
Sourav <sourav.sharma855@gmail.com>
2023-06-09 10:42:01 +0530

    wrote data analysis functiion in utils

---
+++ src/Utils/GeneralUtils.ts
@@ -0,0 +1,38 @@
+import { IRawData } from "../Models";
+
+export const dataAnalysis = (rawData: Array<IRawData>, name: string) => {
+  var result = rawData.reduce(function (r: any, a: any) {
+    r[a[name]] = r[a[name]] || [];
+    r[a[name]].push({
+      cost: parseInt(a.Cost),
+      consumedQuantity: parseInt(a.ConsumedQuantity),
+    });
+    return r;
+  }, Object.create(null));
+
+  let dataForAnalysis: any = [];
+
+  Object.keys(result).forEach(function (key) {
+    dataForAnalysis.push({
+      [name]: key,
+      totalCost: totalCountOfKeyValueFormArray(result[key], "cost"),
+      totalConsumedQuantity: totalCountOfKeyValueFormArray(result[key], "consumedQuantity"),
+    });
+  });
+
+  return dataForAnalysis;
+};
+
+export const totalCountOfKeyValueFormArray = (array: Array<IRawData>, keyName: string) => {
+  let totalCount = 0;
+  array.map((data: any) => {
+    totalCount = totalCount + data[`${keyName}`];
+    return 0;
+  });
+  return totalCount;
+};
+
+export const findLargestTen = (arr: Array<IRawData>, keyName: string) => {
+  arr.sort((a: any, b: any) => (a.totalCost < b[keyName] ? 1 : a[keyName] > b[keyName] ? -1 : 0));
+  return arr.slice(0, 10);
+};
