939f58c42a28316cc535b0308de163ba4f5cd6e5
Sourav <sourav.sharma855@gmail.com>
2023-06-09 10:43:39 +0530

    wrote models

---
+++ src/Models/index.ts
@@ -0,0 +1,13 @@
+export interface IRawData {
+  ConsumedQuantity: string;
+  Cost: string;
+  Date: string;
+  InstanceId: string;
+  MeterCategory: string;
+  ResourceGroup: string;
+  ResourceLocation: string;
+  Tags: any;
+  UnitOfMeasure: string;
+  Location: string;
+  ServiceName: string;
+}
